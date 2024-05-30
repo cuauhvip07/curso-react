ed395fc9fd755b2a0952509c26c7c094198f273c
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-29 19:55:02 -0600

    Proyecto 3 TypeScript
    Se usa el mismo proyecto del carrito de compras pero ahora se movera hacia ts

---
+++ guittarla-ts/src/components/Guittar.tsx
@@ -0,0 +1,26 @@
+
+// En lugar de poner props.price con destructuring se pone solo price
+export default function Guittar({guitar,addToCart}) {
+
+  const {id,name,price, image, description} = guitar
+
+
+  return (
+    <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div className="col-4">
+            <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" /> 
+        </div>
+        <div className="col-8">
+            <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+            <p>{description}</p>
+            <p className="fw-black text-primary fs-3">${price}</p>
+            <button 
+              type="button"
+              className="btn btn-dark w-100"
+              // Para que no se mande a llamr la funcion se le pone un call back en el onClick
+              onClick={() => addToCart(guitar)}
+            >Agregar al Carrito</button>
+        </div>
+    </div>
+  )
+}
