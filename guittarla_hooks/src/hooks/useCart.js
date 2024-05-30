80c950f93952c1e5b543c2b5895b93b4a3da47f9
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-29 19:05:36 -0600

    Migración del codigo hacia el hook
    Se migra el codigo que estaba en el jsx hacia el hook para que sea mas reutilizable

--- guittarla_hooks/src/hooks/useCart.js
+++ guittarla_hooks/src/hooks/useCart.js
@@ -1,9 +1,87 @@
 
+import { useEffect, useState } from 'react'
+import { db } from '../data/db'
 
 export const useCart = () => {
-    const auth = 'Desde auth';
+    // Comprueba si hay valores en caso de que no, lo inicia con un arreglo vacio
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
+
+    const [data, setData] = useState(db);
+    const [cart, setCart] = useState(initialCart);
+
+    const MAX_COUNT = 5;
+    const MIN_COUNT = 1;
+
+    // Se ocupauseEffect ya que como el state es asincrono, ahora cada que cambie cart se ejecuta el codigo 
+    useEffect(() => {
+        localStorage.setItem('cart',JSON.stringify(cart));
+    },[cart])
+
+    function addToCart(item){
+
+
+        // Validar si el elemento existe
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+
+        if(itemExists >= 0){ // Existe en el carrito
+            const updatedCart = [...cart]
+            updatedCart[itemExists].quantity++
+            setCart(updatedCart)
+        }else{
+            item.quantity = 1
+            setCart([...cart, item])
+        }
+
+       
+    }
+
+    function removeFromCart(id){
+        setCart(prevCart => prevCart.filter( guitar => guitar.id !== id))
+    }
+
+    function increaseQuantity(id){
+        const updatedCart = cart.map( item => {
+            if(item.id === id && item.quantity < MAX_COUNT){
+                return {
+                    // Retorna el item y por separado el valor del item
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function decreaseQuantity(id){
+        const updateCart = cart.map( item => {
+            if(item.id === id && item.quantity > MIN_COUNT){
+
+                return{
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            }
+            return item
+        })
+        setCart(updateCart)
+    }
+
+    function resetCar(){
+        setCart([])
+    }
+
 
     return {
-        auth,
+        data,
+        cart,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+        resetCar,
     }
 }
