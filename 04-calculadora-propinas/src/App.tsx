c6c72543c76ca2c3613aa9735b90678e802fc7ce
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-31 12:39:39 -0600

    Listado productos del menu
    Se listan los productos del menú con su información

--- 04-calculadora-propinas/src/App.tsx
+++ 04-calculadora-propinas/src/App.tsx
@@ -11,13 +11,18 @@ function App() {
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
 
-        <div>
-          <h2>Menu</h2>
-          {menuItems.map(item => (
-            <MenuItem
-              key={item.id}
-            />
-          ))}
+        <div className=" p-5">
+          <h2 className=" text-4xl font-black">Menu</h2>
+
+          <div className=" space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
+          
         </div>
 
         <div>
--- 04-calculadora-propinas/src/components/MenuItem.tsx
+++ 04-calculadora-propinas/src/components/MenuItem.tsx
@@ -1,9 +1,17 @@
 
+import type { MenuItem } from "../types"
 
-export default function MenuItem() {
+type MenuItemProps = {
+    item: MenuItem;
+}
+
+export default function MenuItem({item} : MenuItemProps) {
   return (
-    <div>
-      Desde menu item
-    </div>
+    <button
+        className=" border-2 border-teal-200 w-full p-3 flex justify-between hover:bg-teal-200"
+    >
+        <p>{item.name}</p>
+        <p className=" font-black">${item.price}</p>
+    </button>
   )
 }
