8d2bd8f20fe80d7b27c7611513b24cd0d01495b1
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-31 18:30:27 -0600

    Añadiendo elementos al state de orden
    Se añaden los elementos al state con la informacion cuando se presiona en el menu

--- 04-calculadora-propinas/src/hooks/useOrder.ts
+++ 04-calculadora-propinas/src/hooks/useOrder.ts
@@ -4,11 +4,16 @@ import type { MenuItem, OrderItem } from "../types";
 
 export default function useOrder(){
 
-    const {order, setOrder} = useState<OrderItem[]>([]);
+    const [order, setOrder] = useState<OrderItem[]>([]);
 
     const addItem = (item : MenuItem) => {
-        console.log(item)
+        
+        // Se le pasa de esta maera ya que setOrder necesita el quantity
+        const newItem : OrderItem = {...item, quantity: 1}
+        setOrder([...order, newItem])
     }
+
+    console.log(order)
 
     return{
         addItem
