89793c4501de125333499d045d84fea3859b08f5
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-31 20:32:49 -0600

    Mostrando contenido y accion borrar
    Se implementa la x para poder eliminar los articulos y se termina de mostrar toda la información de los productos

--- 04-calculadora-propinas/src/components/OrderContent.tsx
+++ 04-calculadora-propinas/src/components/OrderContent.tsx
@@ -1,4 +1,5 @@
 import { OrderItem } from "../types"
+import { formartCurrency } from "../helpers"
 
 type OrderContentProps = {
     order: OrderItem[]
@@ -10,13 +11,24 @@ export default function OrderContent({order} : OrderContentProps) {
     <div>
         <h2 className="font-black text-4xl">Consumo</h2>
 
-        <div className="space-y-3 mt-5">
+        <div className="space-y-3 mt-10">
             {order.length === 0 ? 
                 <p className=" text-center">La orden esta vacia</p>
             :
                 ( order.map(item => (
-                    <div key={item.id}>
-                        <p>{item.name}</p>
+                    <div 
+                        key={item.id}
+                        className="flex justify-between border-t border-gray-200 py-5 last-of-type:border-b items-center"    
+                    >
+                       <div>
+                            <p className="text-lg">{item.name} - {formartCurrency(item.price)}</p>
+                            <p className=" font-black">Cantidad: {item.quantity} - {formartCurrency(item.price * item.quantity)}</p>
+                       </div>
+                        <button 
+                          className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+                        > 
+                        X 
+                        </button>
                     </div>
                 )))
                 
