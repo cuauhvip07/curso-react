d4e2cd109a8e6138ad145393537b5c58c3c7ebc9
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-31 18:56:58 -0600

    Evitando duplicados
    Se evitan los duplicados y se puede aumenta el numero de ordenes que el cliente desee

--- 04-calculadora-propinas/src/hooks/useOrder.ts
+++ 04-calculadora-propinas/src/hooks/useOrder.ts
@@ -7,10 +7,22 @@ export default function useOrder(){
     const [order, setOrder] = useState<OrderItem[]>([]);
 
     const addItem = (item : MenuItem) => {
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+
+        if(itemExist){
+            const updatedOrder = order.map(
+                orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity : orderItem.quantity + 1} 
+                : orderItem
+            )
+            setOrder(updatedOrder)
+        }else{
+            // Se le pasa de esta maera ya que setOrder necesita el quantity
+            const newItem : OrderItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
         
-        // Se le pasa de esta maera ya que setOrder necesita el quantity
-        const newItem : OrderItem = {...item, quantity: 1}
-        setOrder([...order, newItem])
+        
     }
 
     console.log(order)
