ef2fb914a1a6425637e1478708cec3d1e41bbe73
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-06 19:36:43 -0600

    Definiendo las acciones del reducer
    Se definen las acciones que tendra el reducer

--- 05-calorie-tracker/src/reducers/activity-reducer.ts
+++ 05-calorie-tracker/src/reducers/activity-reducer.ts
@@ -4,14 +4,22 @@ type ActivityState = {
     activities : Activity[]
 }
 
-export type ActivityActions = {
-
-}
+export type ActivityActions = 
+    // El type describe que es lo que esta sucediendo
+    // payload son los datos que se va agregando en el state
+    {type: 'save-activity', payload: {newActivity : Activity}}
 
 export const initialState : ActivityState = {
     activities: []
 }
 
-export const activityReducer = (state : ActivityState = initialState, action : ActivityActions) => {
-
+export const activityReducer = (
+    state : ActivityState = initialState, 
+    action : ActivityActions
+    ) => {
+    
+    if(action.type === 'save-activity'){
+        // Este codigo maneja la logica para actualizar el state
+        console.log('Desde el type de save-activity')
+    }
 }
