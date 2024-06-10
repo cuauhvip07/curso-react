7feef1be7dd1188293bb51f25452d882c1663bec
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-09 20:25:13 -0600

    useReduce
    Se ve el uso de useReducer y como implementarlo

--- 05-calorie-tracker/src/components/Form.tsx
+++ 05-calorie-tracker/src/components/Form.tsx
@@ -1,9 +1,14 @@
 
 import { categories } from "../data/categories"
-import { useState,ChangeEvent, FormEvent } from "react"
+import { useState,ChangeEvent, FormEvent, Dispatch } from "react"
 import { Activity } from "../types"
+import { ActivityActions } from "../reducers/activity-reducer"
 
-export default function Form() {
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>
+}
+
+export default function Form({dispatch} : FormProps) {
 
   const [activity, setActivity] = useState<Activity>({
     category: 1,
@@ -31,7 +36,7 @@ export default function Form() {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement> ) => {
     e.preventDefault()
-    console.log('handle submit')
+    dispatch({type: 'save-activity', payload: {newActivity : activity}})
   }
 
   return (
