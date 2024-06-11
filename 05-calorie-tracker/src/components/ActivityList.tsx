96ee71e67949ae09704ffd3e4a586a43acebd819
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-10 20:27:03 -0600

    Añadiendo categoria y color
    Dependiendo la categoria se le añade el color de fondo, y se ve ahora la categoria por su nombre y no por numero

--- 05-calorie-tracker/src/components/ActivityList.tsx
+++ 05-calorie-tracker/src/components/ActivityList.tsx
@@ -1,4 +1,6 @@
 import { Activity } from "../types"
+import { categories } from "../data/categories";
+import { useMemo } from "react";
 
 type ActivityListProps = {
     activities: Activity[]
@@ -6,19 +8,19 @@ type ActivityListProps = {
 
 
 export default function ActivityList({activities} : ActivityListProps) {
-
-    console.log(activities);
+    // Traer el nombre de la categoria y no el numero
+    const categoryName = useMemo(() => (category : Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
 
   return (
    <>
-    <h2 className=" text-4xl font-bold text-slate-600 text-center">Coimida y Actividades</h2>
+    <h2 className=" text-4xl font-bold text-slate-600 text-center">Comida y Actividades</h2>
 
     {activities.map(activity => (
         <div key={activity.id} className=" px-5 py-10 bg-white mt-5 flex justify-between">
             <div className=" space-y-2 relative">
-                <p>{activity.category}</p>
+                <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>{categoryName(+activity.category)}</p>
                 <p className=" text-2xl font-bold pt-5">{activity.name}</p>
-                <p className=" font-black text-4xl text-lime-500"> {activity.calories}<span>Calorias</span> </p>
+                <p className=" font-black text-4xl text-lime-500"> {activity.calories}<span> Calorias</span> </p>
             </div>
 
             <div className="">
