5719242d4769f70ad9509c78a9b7c2f55a96ca06
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-12 12:47:16 -0600

    Editar las actividades
    En caso de editar la actividad este ya no lo sobrescribe, ya se pueden poner varias actividades y editarlas

--- 05-calorie-tracker/src/reducers/activity-reducer.ts
+++ 05-calorie-tracker/src/reducers/activity-reducer.ts
@@ -27,13 +27,23 @@ export const activityReducer = (
     
     if(action.type === 'save-activity'){
         // Este codigo maneja toda la logica para actualizar el estado
+
+        let updatedActivities : Activity[] = []
+
+        // Si el activeId tiene un id, entonces se ejecuta el codigo
+        if(state.activeId){
+           updatedActivities = state.activities.map( activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+        }else{
+            // action.payload... te trea la informacion del formulario
+            updatedActivities = [...state.activities, action.payload.newActivity]
+        }
         
         
         return {
             // Obtenemos una copia del state para no perder la referncia de los datos
             ...state,
-            // action.payload... te trea la informacion del formulario
-            activities: [...state.activities, action.payload.newActivity]
+            activities: updatedActivities,
+            activeId: '',
         }
     }
 
