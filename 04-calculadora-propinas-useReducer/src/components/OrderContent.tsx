007a9316053ebbc2c907951c853f913055667890
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-14 11:29:16 -0600

    Inicio de proyecto hacia useReducer
    Se pasara el proyecto ya construido con anterioridad hacia useReducer

---
+++ 04-calculadora-propinas-useReducer/src/components/OrderContent.tsx
@@ -0,0 +1,37 @@
+import { OrderItem, MenuItem } from "../types"
+import { formartCurrency } from "../helpers"
+
+type OrderContentProps = {
+    order: OrderItem[]
+    removeItem: (id :MenuItem['id']) => void
+}
+
+
+export default function OrderContent({order,removeItem} : OrderContentProps) {
+  return (
+    <div>
+        <h2 className="font-black text-4xl">Consumo</h2>
+
+        <div className="space-y-3 mt-10">
+            { order.map(item => (
+                    <div 
+                        key={item.id}
+                        className="flex justify-between border-t border-gray-200 py-5 last-of-type:border-b items-center"    
+                    >
+                       <div>
+                            <p className="text-lg">{item.name} - {formartCurrency(item.price)}</p>
+                            <p className=" font-black">Cantidad: {item.quantity} - {formartCurrency(item.price * item.quantity)}</p>
+                       </div>
+                        <button 
+                          className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+                          onClick={() => removeItem(item.id)}
+                        > 
+                        X 
+                        </button>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
