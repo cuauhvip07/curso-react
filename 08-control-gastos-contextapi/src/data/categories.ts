76ebba4ddb5cbf2a59f2597e1fc28abb4f6e5a7e
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-18 14:03:36 -0600

    Creación formulario de gastos

---
+++ 08-control-gastos-contextapi/src/data/categories.ts
@@ -0,0 +1,16 @@
+
+type categoriesProps = {
+    id: string
+    name: string
+    icon: string
+}
+
+export const categories : categoriesProps[] = [
+    { id: '1', name: 'Ahorro', icon: 'ahorro' },
+    { id: '2', name: 'Comida', icon: 'comida' },
+    { id: '3', name: 'Casa', icon: 'casa' },
+    { id: '4', name: 'Gastos Varios', icon: 'gastos' },
+    { id: '5', name: 'Ocio', icon: 'ocio' },
+    { id: '6', name: 'Salud', icon: 'salud' },
+    { id: '7', name: 'Suscripciones', icon: 'suscripciones' },
+  ];
