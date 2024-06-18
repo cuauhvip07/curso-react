ad59799894adf46f475e17b5fff04c9f14373ff8
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-17 19:18:20 -0600

    Creación formulario para definir presupuesto
    Creacion del formulario y useState para almacenar la informacion del formulario

--- 08-control-gastos-contextapi/src/components/BudgetForm.tsx
+++ 08-control-gastos-contextapi/src/components/BudgetForm.tsx
@@ -1,11 +1,40 @@
 
+import { useState, ChangeEvent } from "react"
 
-import React from 'react'
+
 
 export default function BudgetForm() {
+
+    const [budget, setBudget] = useState(0);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+    }
+
   return (
-    <div>
-      BudgetFomr
-    </div>
+    <form action="" className=" space-y-5 ">
+
+        <div className="flex flex-col space-y-5">
+            <label htmlFor="budget" className=" text-4xl text-blue-600 font-bold text-center">
+                Definir Presupuesto
+            </label>
+            <input 
+                type="number" 
+                className=" w-full bg-white border border-gray-200 p-2"
+                placeholder="Define tu presupuesto"
+                name="budget"
+                id="budget"
+                value={budget}
+                onChange={ handleChange}
+            />
+        </div>
+
+        <input 
+            type="submit" 
+            value='Definir Presupuesto'
+            className=" bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+        />
+
+    </form>
   )
 }
