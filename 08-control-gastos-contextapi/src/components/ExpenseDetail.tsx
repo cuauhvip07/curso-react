373d8eff420699c23a555d4cdfca551b58fe6c0c
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-19 13:17:09 -0600

    Mostrando iconos
    Se muestran los iconos dependiendo la categoria de los gastos

--- 08-control-gastos-contextapi/src/components/ExpenseDetail.tsx
+++ 08-control-gastos-contextapi/src/components/ExpenseDetail.tsx
@@ -1,6 +1,8 @@
+import { useMemo } from "react"
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { categories } from "../data/categories"
 
 type ExpenseDetailProps = {
     expense: Expense
@@ -8,13 +10,18 @@ type ExpenseDetailProps = {
 
 
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+
+    // Traer la informacion de la categoria y aparezca el tipo de categoria en la vista
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0] ,[expense])
+
   return (
     <div className=" bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
         <div className="">
-
+            <img src={`/icono_${categoryInfo.icon}.svg`} alt="Icono gasto" className=" w-20" />
         </div>
 
-        <div>
+        <div className=" flex-1 space-y-2">
+            <p className=" text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
             <p>{expense.expenseName}</p>
             <p className=" text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
         </div>
