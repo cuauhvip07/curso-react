8c29b82452cd6eb9b0730e97b0d66a98de92cbe3
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-19 20:37:15 -0600

    Llenar el formulario del gasto
    En caso de editar un producto, el formulario se llena con los datos ya establecidos

--- 08-control-gastos-contextapi/src/components/ExpenseForm.tsx
+++ 08-control-gastos-contextapi/src/components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent, useEffect } from "react";
 import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker'
@@ -23,7 +23,14 @@ export default function ExpenseForm() {
     })
 
     const [error,setError] = useState('');
-    const {dispatch} = useBudget();
+    const {state,dispatch} = useBudget();
+
+    useEffect(() => {
+        if(state.editingId){
+            const editingExpense = state.expenses.filter( expense => expense.id === state.editingId)[0]
+            setExpense(editingExpense)
+        }
+    },[state.editingId])
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
         const {name,value} = e.target
