e2b65750b681e7d60d1308c6916028b62adb7227
cuauhvip07 <cuauvillalba1@gmail.com>
2024-07-01 21:49:54 -0600

    Modificando los state de zustand
    Se modifica el state para poder agregar un id unico a cada paciente.

--- 10-pacientes-zustand/src/store.ts
+++ 10-pacientes-zustand/src/store.ts
@@ -1,17 +1,29 @@
 
 import { create } from 'zustand'
 import { DraftPatient, Patient } from './types'
+import {v4 as uuidv4} from 'uuid'
 
 type PatientState = {
     patients: Patient[],
     addPatient: (data:DraftPatient) => void
 }
 
-export const usePatientStore = create<PatientState>(() => ({
+const createPatient = (patient: DraftPatient) : Patient => {
+    return {...patient, id:uuidv4()}
+}
+
+// Se puede agregar set y get para obtener o modifcar las funciones
+export const usePatientStore = create<PatientState>((set) => ({
     // Se coloca el state como las funciones que modifican el state
 
     patients: [],
+
     addPatient: (data) => {
-        console.log(data)
+        
+        const newPatient = createPatient(data)
+        // El state recupera el estado de las funciones dentro de usePatientStore
+        set((state) => ({
+            patients: [...state.patients, newPatient]
+        }))
     }
 }))
