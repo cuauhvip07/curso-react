1e35ee899ad239052255b890e0ca91ecb66c6eda
cuauhvip07 <cuauvillalba1@gmail.com>
2024-07-04 11:21:20 -0600

    Type Guards
    Segunda manera para tipar una API

--- 11-clima-ts/src/hooks/useWeather.ts
+++ 11-clima-ts/src/hooks/useWeather.ts
@@ -2,6 +2,19 @@
 import axios from "axios"
 import { SearchType, Weather } from "../types"
 
+
+// 2. Type Guards
+// function isWeatherResponse(weather : unknown) : weather is Weather  {
+//     return(
+//         Boolean(weather) && 
+//         typeof weather === 'object' &&
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' &&
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number'
+//     )
+// }
+
 export default function useWeather(){
 
     const fetchWeather = async (search : SearchType) => {
@@ -24,9 +37,12 @@ export default function useWeather(){
             // 1. Castear un valor
             // const {data : weatherResult} = await axios<Weather>(weatherUrl)
 
-
-            const {data : weatherResult} = await axios(weatherUrl)
-            console.log(weatherResult)
+            // 2. Type Guards
+            // const {data : weatherResult} = await axios(weatherUrl)
+            // const result = isWeatherResponse(weatherResult)
+            // if(result) {
+            //     console.log(weatherResult.name)
+            // }
 
         } catch (error) {
             console.log(error)
