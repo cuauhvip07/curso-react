dec58c03710856242565aa167133c752876aab1b
cuauhvip07 <cuauvillalba1@gmail.com>
2024-07-05 09:46:19 -0600

    Creación formulario
    Se pone el formulario de manera condicional para que aparezca solo en el inicio

--- 13-bebidas-RRouterDom/src/components/Header.tsx
+++ 13-bebidas-RRouterDom/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 
@@ -6,7 +7,9 @@ import { NavLink, useLocation } from "react-router-dom";
 export default function Header() {
 
     const location = useLocation()
-    console.log(location.pathname)
+    
+    // Comprobar si esta en la pagina de inicio
+    const isHome = useMemo(() => location.pathname === '/' ,[location.pathname])
 
   return (
 
@@ -39,6 +42,51 @@ export default function Header() {
                     </NavLink>
                 </nav>
             </div>
+
+            {isHome && (
+                <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 mt-32 p-10 rounded-lg shadow space-y-6">
+                    <div className=" space-y-4">
+                        <label 
+                            htmlFor="ingredient"
+                            className=" block text-white uppercase font-extrabold text-lg"
+                        >
+                            Nombre o Ingredientes
+                        </label>
+
+                        <input 
+                            type="text" 
+                            id="ingredient"
+                            name="ingredient"
+                            className="p-3 w-full rounded-lg focus:outline-none"
+                            placeholder="Nombre o Ingrediente. Ej: Vodka, Tequila, Café"
+                        />
+                    </div>
+
+                    <div className=" space-y-4">
+                        <label 
+                            htmlFor="category"
+                            className=" block text-white uppercase font-extrabold text-lg"
+                        >
+                            Categoria
+                        </label>
+
+                        <select 
+                            name="category" 
+                            id="category"
+                            className="p-3 w-full rounded-lg focus:outline-none text-gray-500"
+                        >
+                            <option value="">-- Seleccione --</option>
+                        </select>
+                    </div>
+
+                    <input 
+                        type="submit" 
+                        value='Buscar Recetas'
+                        className=" cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+                    />
+
+                </form>
+            )}
         </div>
     </header>
   )
