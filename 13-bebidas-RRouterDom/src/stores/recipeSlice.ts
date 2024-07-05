65fa5502736c78cea22023974270ee383592f942
cuauhvip07 <cuauvillalba1@gmail.com>
2024-07-05 12:11:17 -0600

    Primeros pasos con multiples stores
    Se tipan los elementos para tener un mejor autocompletado

--- 13-bebidas-RRouterDom/src/stores/recipeSlice.ts
+++ 13-bebidas-RRouterDom/src/stores/recipeSlice.ts
@@ -1,5 +1,12 @@
+import { StateCreator } from "zustand"
 
+type Category = {}
 
-export const createRecipesSlice = () => ({
+export type RecipesSliceTypes = {
+    categories: Category[]
+}
+
+// Se le agrega el StateCreator para tiparlo
+export const createRecipesSlice : StateCreator<RecipesSliceTypes> = () => ({
     categories:[]
 })
--- 13-bebidas-RRouterDom/src/views/IndexPage.tsx
+++ 13-bebidas-RRouterDom/src/views/IndexPage.tsx
@@ -3,6 +3,9 @@ import { useAppStore } from "../stores/useAppStore"
 
 
 export default function IndexPage() {
+
+  useAppStore((state) => state.categories)
+
   return (
     <>
       <h1>Inicio</h1>
