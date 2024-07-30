b9ef1b69ac1fb7196ffa116bbbc773447c3176d5
cuauhvip07 <cuauvillalba1@gmail.com>
2024-07-30 15:07:29 -0600

    Comentarios uso API
    Se agregan comentarios para saber como tipar la información de una API

--- 13-bebidas-RRouterDom/src/types/index.ts
+++ 13-bebidas-RRouterDom/src/types/index.ts
@@ -2,6 +2,8 @@
 import { z } from 'zod'
 import { CategoriesAPIResponseSchema, DrinksAPIResponse, SearchFilterSchema } from '../utils/recipes-schema'
 
+// Inferir los types de los schemas
+
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
