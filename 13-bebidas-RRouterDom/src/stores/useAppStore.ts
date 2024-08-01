27d97284958d5b2a3b0dd3fd43adf8479883b35a
cuauhvip07 <cuauvillalba1@gmail.com>
2024-08-01 17:01:42 -0600

    Slice de favoritos
    Se crea el slice de favoritos para separar el código. Se declara en el useAppStore

--- 13-bebidas-RRouterDom/src/stores/useAppStore.ts
+++ 13-bebidas-RRouterDom/src/stores/useAppStore.ts
@@ -2,10 +2,12 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { RecipesSliceTypes, createRecipesSlice } from './recipeSlice'
+import { createFavoritesSlice, FavoritesSliceType } from './favoritesSlice'
 
 
 // ...a manda a traer todas las copias (set,get,api) -> Solo se pueden poner en el create
-export const useAppStore = create<RecipesSliceTypes>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceTypes & FavoritesSliceType>()(devtools((...a) => ({
     // Se manda a llamar
-    ...createRecipesSlice(...a)
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a)
 })))
