1b5b3e8c0c7ababc44501be347d06b57ceeb567b
cuauhvip07 <cuauvillalba1@gmail.com>
2024-08-13 01:11:07 -0600

    Conexion bd con variables de entorno
    Para que la url de la base de datos no este expuesta, esta se pasa a una variable de entorno y utilizando la dependencia de dotenv se trae la información de la variable

--- 14-PERN-Admin-Productos/rest_api_node_ts_server/src/config/db.ts
+++ 14-PERN-Admin-Productos/rest_api_node_ts_server/src/config/db.ts
@@ -1,5 +1,9 @@
 import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const db = new Sequelize('postgresql://rest_api_node_ts_zdi7_user:fddjHlG0MaFRUuZ6i9YIgN3cGAAgnSxu@dpg-cqtbskrv2p9s73dd6120-a.oregon-postgres.render.com/rest_api_node_ts_zdi7?ssl=true') // Se le agrega el ?ssl=true para que ya no de error a la conexión de la bd
+
+// console.log(process.env)  nos da las varibales y para acceder a la que creamos en .env le ponemos el nombre
+const db = new Sequelize(process.env.DATABASE_URL) // Se le agrega el ?ssl=true para que ya no de error a la conexión de la bd
 
 export default db;
