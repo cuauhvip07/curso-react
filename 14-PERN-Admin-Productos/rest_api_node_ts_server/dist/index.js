0ff5f78685ed3900a44cd33950a1e413d7d21bd0
Cuauhtemoc Villalba <134470510+cuauhvip07@users.noreply.github.com>
2024-08-13 01:13:14 -0600

    Delete 14-PERN-Admin-Productos/rest_api_node_ts_server/dist directory

--- 14-PERN-Admin-Productos/rest_api_node_ts_server/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-(0, server_1.sumar)();
-console.log('si');
-//# sourceMappingURL=index.js.map
