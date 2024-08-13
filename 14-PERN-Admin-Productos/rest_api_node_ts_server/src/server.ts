6fccd78da1084c8a420d021250090e22d630e8fb
cuauhvip07 <cuauvillalba1@gmail.com>
2024-08-12 19:01:12 -0600

    Metodos http
    Se ven los diferentes métodos que vamos a estar utilizando en el back

--- 14-PERN-Admin-Productos/rest_api_node_ts_server/src/server.ts
+++ 14-PERN-Admin-Productos/rest_api_node_ts_server/src/server.ts
@@ -6,13 +6,29 @@ const server = express()
 // Routing 
 server.get('/', (req,res) => {
 
-    const data = [
-        {id: 1, nombre: 'Cuauh'},
-        {id: 2, nombre: 'Villalba'}
-    ]
-
     // Mandar datos hacia la pantalla
-    res.send(data)
+    // res.send(data)    Esta es otra manera para mandar los datos
+    res.json('Desde get')
+})
+
+server.post('/', (req,res) => {
+
+    res.json('Desde post')
+})
+
+server.put('/', (req,res) => {
+
+    res.json('Desde put')
+})
+
+server.patch('/', (req,res) => {
+
+    res.json('Desde patch')
+})
+
+server.delete('/', (req,res) => {
+
+    res.json('Desde delete')
 })
 
 export default server
