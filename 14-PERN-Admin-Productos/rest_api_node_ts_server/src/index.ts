d1ff7ac8ce5b371b286c6b75ff9090d1fe2209bf
cuauhvip07 <cuauvillalba1@gmail.com>
2024-09-21 15:49:47 -0600

    Reinicio de curso

--- 14-PERN-Admin-Productos/rest_api_node_ts_server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import colors from 'colors'
-import server from "./server";
-
-// La asignacion que nos da cuando se suba a produccion
-const port = process.env.port || 4000
-
-server.listen(port, () => {
-    console.log(colors.cyan.bold(`REST API en el puerto ${port}`))
-})
