f6ffd8cd9a9278ad9f9bfa0d857e3ccadeb0ffd0
cuauhvip07 <cuauvillalba1@gmail.com>
2024-10-14 20:13:06 -0600

    Validación crear producto
    Se crea la validación para poder crear un producto

--- 14-PERN-Admin-Productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
+++ 14-PERN-Admin-Productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
@@ -2,6 +2,17 @@ import request from 'supertest'
 import server from '../../server'
 
 describe('POST /api/products', () => {
+    // Segunda parte de validacion
+    it('should display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({}) // Validar cuando no se envia nada
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+    // Primera parte de validacion
     it('should create a new product', async () => {
         const response = await request(server).post('/api/products').send(
             {
