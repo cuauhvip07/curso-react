72207450b89485b16ef4372def65e487a8969e57
cuauhvip07 <cuauvillalba1@gmail.com>
2024-12-13 16:40:45 -0600

    Acciones del Data Router
    Se implementa la acción en el formulario, comunicandolo con el router

--- 14-PERN-Admin-Productos/client/src/views/NewProduct.tsx
+++ 14-PERN-Admin-Productos/client/src/views/NewProduct.tsx
@@ -1,5 +1,12 @@
 
-import { Link } from "react-router-dom"
+import { Link, Form } from "react-router-dom"
+
+
+export async function action (){
+  console.log('Desde action')
+  return {}
+}
+
 
 export default function NewProduct() {
   return (
@@ -13,6 +20,47 @@ export default function NewProduct() {
           Volver a productos
         </Link>
       </div>
+
+      <Form
+        className="mt-10"
+        method="POST"
+        // action=""   // Accion que se añade para que se conecte con el router
+      >
+      
+        <div className="mb-4">
+            <label
+              className="text-gray-800"
+              htmlFor="name"
+            >Nombre Producto:</label>
+            <input 
+              id="name"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Nombre del Producto"
+              name="name"
+            />
+        </div>
+
+        <div className="mb-4">
+            <label
+              className="text-gray-800"
+              htmlFor="price"
+            >Precio:</label>
+            <input 
+              id="price"
+              type="number"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Precio Producto. ej. 200, 300"
+              name="price"
+            />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
+
     </>
   )
 }
