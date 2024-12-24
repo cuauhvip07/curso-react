6dbf07279973cd22278372bd87cd3292bd418406
cuauhvip07 <cuauvillalba1@gmail.com>
2024-12-24 15:34:09 -0600

    Botón de edición de products
    Se crea un botón para poder editar un producto al igual que url dinamica

---
+++ 14-PERN-Admin-Productos/client/src/views/EditProduct.tsx
@@ -0,0 +1,94 @@
+
+import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom"
+import ErrorMessage from "../components/ErrorMessage"
+import { addProduct } from "../services/ProductService"
+
+
+export async function action ({request} : ActionFunctionArgs){ // Request es ncesario
+  // Object.fromEntries Ayuda acceder a la informacion del formData
+  // request.formData() trae la informacion del formulario pero se necesarita la funcion del objet
+  const data = Object.fromEntries(await request.formData())
+  
+  let error = ''
+  if(Object.values(data).includes('')){
+    error = 'Todos los campos son obligatorios'
+  }
+
+  if(error.length){
+    return error // Cuando retornas algo, estan disponibles en el componente (Antes del return)
+  }
+
+  await addProduct(data) 
+
+
+  return redirect('/') // Se debe de retornar algo obligatoriamente o redireccionar al usuario
+}
+
+
+
+export default function EditProduct() {
+
+  const error = useActionData() as string // UsaruseActionData -> Querer tener el resultado de una action (formulario)
+  
+
+  return (
+    <>
+      <div className=" flex justify-between">
+        <h2 className=" text-4xl font-black text-slate-500">Editar Producto</h2>
+        <Link
+          to={'/'}
+          className=" rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+        >
+          Volver a productos
+        </Link>
+      </div>
+
+      {error && 
+        <ErrorMessage>
+          {error}
+        </ErrorMessage>
+      }
+
+      <Form
+        className="mt-10"
+        method="POST"
+        // action=""   // Accion que se añade para que se conecte con el router
+      >
+      
+        <div className="mb-4">
+            <label
+              className="text-gray-800"
+              htmlFor="name"
+            >Nombre Producto:</label>
+            <input 
+              id="name"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Nombre del Producto"
+              name="name"
+            />
+        </div>
+
+        <div className="mb-4">
+            <label
+              className="text-gray-800"
+              htmlFor="price"
+            >Precio:</label>
+            <input 
+              id="price"
+              type="number"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Precio Producto. ej. 200, 300"
+              name="price"
+            />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
+
+    </>
+  )
+}
