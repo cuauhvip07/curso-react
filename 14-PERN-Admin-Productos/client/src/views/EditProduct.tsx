2ba6657f56aeda518fcb658309b1d51b783ab00f
cuauhvip07 <cuauvillalba1@gmail.com>
2024-12-26 23:26:11 -0600

    Agregar disponibiloidad
    Se agrega la disponibilidad en el formulario para poder cambiar

--- 14-PERN-Admin-Productos/client/src/views/EditProduct.tsx
+++ 14-PERN-Admin-Productos/client/src/views/EditProduct.tsx
@@ -45,6 +45,10 @@ export async function action ({request,params} : ActionFunctionArgs){ // Request
   
 }
 
+const availabilityOptions = [
+    { name: 'Disponible', value: true},
+    { name: 'No Disponible', value: false}
+]
 
 
 export default function EditProduct() {
@@ -106,6 +110,27 @@ export default function EditProduct() {
               defaultValue={product.price}
             />
         </div>
+
+
+
+
+        <div className="mb-4">
+            <label
+                className="text-gray-800"
+                htmlFor="availability"
+            >Disponibilidad:</label>
+            <select 
+                id="availability"
+                className="mt-2 block w-full p-3 bg-gray-50"
+                name="availability"
+                defaultValue={product?.availability.toString()}
+            >
+                {availabilityOptions.map(option => (
+                <option key={option.name} value={option.value.toString()}>{option.name}</option>
+                ))}
+            </select>
+        </div>
+
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
