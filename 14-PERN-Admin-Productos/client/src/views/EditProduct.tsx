b92cb6c03f67516fedcf109f8a388aafd54cb0ad
cuauhvip07 <cuauvillalba1@gmail.com>
2024-12-26 23:53:33 -0600

    Componente reutilizable
    Se crea un componente reutilizable para no tener el mismo código en los formularios

--- 14-PERN-Admin-Productos/client/src/views/EditProduct.tsx
+++ 14-PERN-Admin-Productos/client/src/views/EditProduct.tsx
@@ -3,13 +3,13 @@ import { Link, Form, useActionData, ActionFunctionArgs, redirect, LoaderFunction
 import ErrorMessage from "../components/ErrorMessage"
 import { addProduct, getProductbyId, updateProduct } from "../services/ProductService"
 import { Product } from "../types"
+import ProductForm from "../components/ProductForm"
 
 // Traer los parametros del router
 export async function loader({params} : LoaderFunctionArgs){
 
     if(params.id !== undefined){
         const product = await getProductbyId(+params.id)
-        console.log(product)
 
         if(!product){
             return redirect('/')
@@ -81,38 +81,9 @@ export default function EditProduct() {
         // action=""   // Accion que se añade para que se conecte con el router
       >
       
-        <div className="mb-4">
-            <label
-              className="text-gray-800"
-              htmlFor="name"
-            >Nombre Producto:</label>
-            <input 
-              id="name"
-              type="text"
-              className="mt-2 block w-full p-3 bg-gray-50"
-              placeholder="Nombre del Producto"
-              name="name"
-              defaultValue={product.name} // deaultValue es de React-router-dom
-            />
-        </div>
-
-        <div className="mb-4">
-            <label
-              className="text-gray-800"
-              htmlFor="price"
-            >Precio:</label>
-            <input 
-              id="price"
-              type="number"
-              className="mt-2 block w-full p-3 bg-gray-50"
-              placeholder="Precio Producto. ej. 200, 300"
-              name="price"
-              defaultValue={product.price}
-            />
-        </div>
-
-
-
+        <ProductForm
+            product={product}
+        />
 
         <div className="mb-4">
             <label
--- 14-PERN-Admin-Productos/client/src/views/NewProduct.tsx
+++ 14-PERN-Admin-Productos/client/src/views/NewProduct.tsx
@@ -2,6 +2,7 @@
 import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom"
 import ErrorMessage from "../components/ErrorMessage"
 import { addProduct } from "../services/ProductService"
+import ProductForm from "../components/ProductForm"
 
 
 export async function action ({request} : ActionFunctionArgs){ // Request es ncesario
@@ -55,33 +56,8 @@ export default function NewProduct() {
         // action=""   // Accion que se añade para que se conecte con el router
       >
       
-        <div className="mb-4">
-            <label
-              className="text-gray-800"
-              htmlFor="name"
-            >Nombre Producto:</label>
-            <input 
-              id="name"
-              type="text"
-              className="mt-2 block w-full p-3 bg-gray-50"
-              placeholder="Nombre del Producto"
-              name="name"
-            />
-        </div>
+        <ProductForm/>
 
-        <div className="mb-4">
-            <label
-              className="text-gray-800"
-              htmlFor="price"
-            >Precio:</label>
-            <input 
-              id="price"
-              type="number"
-              className="mt-2 block w-full p-3 bg-gray-50"
-              placeholder="Precio Producto. ej. 200, 300"
-              name="price"
-            />
-        </div>
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
