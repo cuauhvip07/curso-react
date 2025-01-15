912a466985492ccce4968cf05ad772dc76ee1a6e
cuauhvip07 <cuauvillalba1@gmail.com>
2025-01-14 21:07:28 -0600

    Revisar que un usaurio exista
    Se verifica que un usuario ya existe

--- cashtracker/backend/src/controllers/AuthController.ts
+++ cashtracker/backend/src/controllers/AuthController.ts
@@ -6,6 +6,17 @@ export class AuthController {
     
     
     static createAccount = async (req: Request,res: Response) =>{
+        const {email} = req.body
+
+        // Prevenir duplicados
+        const userExists = await User.findOne({where: {email: email}}) // Se pone el nombre de la columna
+
+        if(userExists){
+            const error = new Error('El email ya esta registrado')
+            res.status(409).json({error:error.message})
+            return
+        }
+
         try {
             const user = new User(req.body)
             await user.save()
