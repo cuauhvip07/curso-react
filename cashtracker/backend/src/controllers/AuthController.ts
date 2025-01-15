46abfb5c20fde00afa8414b7a35b03df3f3822a3
cuauhvip07 <cuauvillalba1@gmail.com>
2025-01-14 21:02:22 -0600

    Crear una cuenta
    Se pone el código para poder crear una cuenta

--- cashtracker/backend/src/controllers/AuthController.ts
+++ cashtracker/backend/src/controllers/AuthController.ts
@@ -1,11 +1,18 @@
 import { Request,Response } from "express"
+import User from "../models/User"
 
 
 export class AuthController {
     
     
     static createAccount = async (req: Request,res: Response) =>{
-        res.json('Creando cuenta')
+        try {
+            const user = new User(req.body)
+            await user.save()
+            res.json('Cuenta creada correctamente')
+        } catch (error) {
+            res.status(500).json({error:'Hubo un error'})
+        }
     }   
 
 }
