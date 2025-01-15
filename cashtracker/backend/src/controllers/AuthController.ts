4b0551551f273c3382bea762071322e13420df65
cuauhvip07 <cuauvillalba1@gmail.com>
2025-01-14 21:14:07 -0600

    Hashear passwords
    Se crea la función para poder hashear los passwords de las cuentas

--- cashtracker/backend/src/controllers/AuthController.ts
+++ cashtracker/backend/src/controllers/AuthController.ts
@@ -1,12 +1,13 @@
 import { Request,Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 
 export class AuthController {
     
     
     static createAccount = async (req: Request,res: Response) =>{
-        const {email} = req.body
+        const {email, password} = req.body
 
         // Prevenir duplicados
         const userExists = await User.findOne({where: {email: email}}) // Se pone el nombre de la columna
@@ -19,6 +20,7 @@ export class AuthController {
 
         try {
             const user = new User(req.body)
+            user.password = await hashPassword(password)
             await user.save()
             res.json('Cuenta creada correctamente')
         } catch (error) {
