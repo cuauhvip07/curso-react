7906347ed57132d5e2ad1da1c96603766de45c41
cuauhvip07 <cuauvillalba1@gmail.com>
2025-01-14 21:18:14 -0600

    Generación de token
    Se genera un token para poder verificar la cuenta y se guarde en la base de datos

--- cashtracker/backend/src/controllers/AuthController.ts
+++ cashtracker/backend/src/controllers/AuthController.ts
@@ -1,6 +1,7 @@
 import { Request,Response } from "express"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
+import { generateToken } from "../utils/token"
 
 
 export class AuthController {
@@ -21,6 +22,7 @@ export class AuthController {
         try {
             const user = new User(req.body)
             user.password = await hashPassword(password)
+            user.token = generateToken()
             await user.save()
             res.json('Cuenta creada correctamente')
         } catch (error) {
