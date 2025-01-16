335d19acf81a4a46302f73add894b4286e1fccd8
cuauhvip07 <cuauvillalba1@gmail.com>
2025-01-15 21:07:29 -0600

    Creación del archivo para mandar emails

--- cashtracker/backend/src/controllers/AuthController.ts
+++ cashtracker/backend/src/controllers/AuthController.ts
@@ -2,6 +2,7 @@ import { Request,Response } from "express"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
 import { generateToken } from "../utils/token"
+import { AuthEmail } from "../emails/AuthEmail"
 
 
 export class AuthController {
@@ -24,6 +25,13 @@ export class AuthController {
             user.password = await hashPassword(password)
             user.token = generateToken()
             await user.save()
+
+            await AuthEmail.sendConfirmationEmail({
+                name: user.name,
+                email: user.email.valueOf(),
+                token: user.token
+            })
+
             res.json('Cuenta creada correctamente')
         } catch (error) {
             res.status(500).json({error:'Hubo un error'})
---
+++ cashtracker/backend/src/emails/AuthEmail.ts
@@ -0,0 +1,12 @@
+
+type EmailType = {
+    name: string,
+    email: string,
+    token: string
+}
+
+export class AuthEmail {
+    static sendConfirmationEmail = async (user : EmailType) => {
+        console.log(user)
+    }
+}
