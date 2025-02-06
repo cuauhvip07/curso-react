afe789c3409983109e640750d8ee4886d86e9553
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-06 17:01:51 -0600

    Almacenando el nuevo password
    Se almacena el nuevo password del usuario

--- cashtracker/backend/src/controllers/AuthController.ts
+++ cashtracker/backend/src/controllers/AuthController.ts
@@ -145,6 +145,20 @@ export class AuthController {
     }
 
     static updateCurrentUserPassword = async (req:Request,res:Response) => {
-        res.json('updateuserpasword')
+        const {current_password,password} = req.body
+        const {id} = req.user
+
+        const user = await User.findByPk(id)
+
+        const isPasswordCorrect = await checkPassword(current_password,user.password)
+        if(!isPasswordCorrect){
+            const error = new Error('El password actual es incorrecto')
+            res.status(401).json({error:error.message})
+            return
+        }
+
+        user.password = await hashPassword(password)
+        await user.save()
+        res.json('Password actualizado correctamente')
     }
 }
