7136e1b3fe43cf926008ed23f83182ef58835882
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-07 12:24:29 -0600

    Check password
    Esta ruta se ocupara para que el usuario en caso de querer eliminar un gasto, se pida la contraseña para saber si en realidad si lo quiere eliminar

--- cashtracker/backend/src/controllers/AuthController.ts
+++ cashtracker/backend/src/controllers/AuthController.ts
@@ -161,4 +161,21 @@ export class AuthController {
         await user.save()
         res.json('Password actualizado correctamente')
     }
+
+    static checkPassword = async (req:Request,res:Response) => {
+        const {password} = req.body
+        const {id} = req.user
+
+        const user = await User.findByPk(id)
+
+        const isPasswordCorrect = await checkPassword(password,user.password)
+        if(!isPasswordCorrect){
+            const error = new Error('El password es incorrecto')
+            res.status(401).json({error:error.message})
+            return
+        }
+
+        res.json('Password correcto')
+    }
+
 }
--- cashtracker/backend/src/routes/authRouter.ts
+++ cashtracker/backend/src/routes/authRouter.ts
@@ -62,5 +62,13 @@ router.post('/update-password',
     AuthController.updateCurrentUserPassword
 )
 
+router.post('/check-password',
+    authenticate,
+    body('password')
+        .notEmpty().withMessage('El password no puede ir vacio'),
+    handleInputErrors,
+    AuthController.checkPassword
+)
+
 
 export default router
