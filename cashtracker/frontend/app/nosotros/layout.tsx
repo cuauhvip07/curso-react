939ad2802ca4a1c27264ae14dcce8d73ef3c526a
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-07 18:33:57 -0600

    Uso de layouts
    Se ve el uso de los layouts en los archivos de nextjs

---
+++ cashtracker/frontend/app/nosotros/layout.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+export default function NosotrosLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <>
+            <h1>Desde laypoutNosotros</h1>
+            {children}
+        </>
+    );
+  }
---
+++ cashtracker/frontend/app/nosotros/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function NosotrosPage() {
+  return (
+    <div>
+      Nosotros
+    </div>
+  )
+}
