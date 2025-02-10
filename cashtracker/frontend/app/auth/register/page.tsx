9b4dd864a50a2525e68722d9f78149f561a93fcb
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-09 20:02:15 -0600

    Metadata
    Se añade el metadata a la pagina para agregar la información

--- cashtracker/frontend/app/auth/register/page.tsx
+++ cashtracker/frontend/app/auth/register/page.tsx
@@ -1,17 +1,24 @@
+import type { Metadata } from "next";
 import RegisterForm from "@/components/auth/RegisterForm"
+
+
+export const metadata: Metadata = {
+  title: "CashTracker - Crear cuenta",
+  description: "CashTracker - Crear cuenta"
+}
 
 export default function RegisterPage() {
 
-    console.log('Register pagw')
+  console.log('Register pagw')
 
   return (
     <div>
-        <>
-            <h1 className=" font-black text-6xl text-purple-950">Crea una cuenta</h1>
-            <p className="text-3xl font-bold">y controla tus <span className=" text-amber-500">finanzas</span></p>
+      <>
+        <h1 className=" font-black text-6xl text-purple-950">Crea una cuenta</h1>
+        <p className="text-3xl font-bold">y controla tus <span className=" text-amber-500">finanzas</span></p>
 
-            <RegisterForm/>
-        </>
+        <RegisterForm />
+      </>
     </div>
   )
 }
