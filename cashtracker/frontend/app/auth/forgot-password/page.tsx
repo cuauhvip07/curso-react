a23eb7e0fad478039f8c96dfde040a95dbd93752
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-09 21:03:50 -0600

    Routing
    Se hace la conexión hacia las paginas ya creadas en cada una de ellas

--- cashtracker/frontend/app/auth/forgot-password/page.tsx
+++ cashtracker/frontend/app/auth/forgot-password/page.tsx
@@ -1,6 +1,6 @@
 import ForgotPasswordForm from "@/components/auth/ForgotPasswordForm";
 import type { Metadata } from "next";
-
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
@@ -16,7 +16,24 @@ export default function ForgotPassword() {
                 <h1 className=" font-black text-6xl text-purple-950">¿Olvidaste tu contraseña?</h1>
                 <p className="text-3xl font-bold">aqui puedes <span className=" text-amber-500">reestablecerla</span></p>
 
-                <ForgotPasswordForm/>
+                <ForgotPasswordForm />
+
+                <nav className=" mt-10 flex flex-col space-y-4">
+                    <Link
+                        href="/auth/login"
+                        className=" text-center text-gray-500"
+                    >
+                        ¿Ya tienes cuenta? Inicia sesión
+                    </Link>
+
+                    <Link
+                        href="/auth/register"
+                        className=" text-center text-gray-500"
+                    >
+                        ¿No tienes cuenta? Crea una
+                    </Link>
+
+                </nav>
 
 
             </>
--- cashtracker/frontend/app/auth/register/page.tsx
+++ cashtracker/frontend/app/auth/register/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import RegisterForm from "@/components/auth/RegisterForm"
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
@@ -18,6 +19,23 @@ export default function RegisterPage() {
         <p className="text-3xl font-bold">y controla tus <span className=" text-amber-500">finanzas</span></p>
 
         <RegisterForm />
+
+        <nav className=" mt-10 flex flex-col space-y-4">
+          <Link 
+            href="/auth/login"
+            className=" text-center text-gray-500"
+          >
+            ¿Ya tienes cuenta? Inicia sesión
+          </Link>
+
+          <Link 
+            href="/auth/forgot-password"
+            className=" text-center text-gray-500"
+          >
+            ¿Olvidaste tu contraseña? Reestablecer
+          </Link>
+
+        </nav>
       </>
     </div>
   )
