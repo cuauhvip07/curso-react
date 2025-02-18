b7c4c9df76776b5d8c5cb1e3fd99fcf1744ed6a4
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-18 16:21:45 -0600

    Update layout.tsx

--- cashtracker/frontend/app/auth/layout.tsx
+++ cashtracker/frontend/app/auth/layout.tsx
@@ -10,17 +10,17 @@ export default function AuthLayout({
         <>
             <div className=" lg:grid lg:grid-cols-2 lg:min-h-screen">
 
-            <div className=" bg-purple-950 flex justify-center lg:bg-auth lg:bg-no-repeat bg-30 bg-left-bottom">
-                <div className=" w-96 py-10 lg:py-20">
-                    <Logo/>
+                <div className=" bg-purple-950 flex justify-center lg:bg-auth lg:bg-no-repeat bg-30 bg-left-bottom">
+                    <div className=" w-96 py-10 lg:py-20">
+                        <Logo/>
+                    </div>
                 </div>
-            </div>
 
-            <div className=" p-10 lg:py-28">
-                <div className=" max-w-3xl mx-auto">
-                    {children}
+                <div className=" p-10 lg:py-28">
+                    <div className=" max-w-3xl mx-auto">
+                        {children}
+                    </div>
                 </div>
-            </div>
 
             </div>
 
