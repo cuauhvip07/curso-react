cee1a07079f480b2eeedfbb33a0975df2019aeb4
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-21 18:41:22 -0600

    Redireccionar
    Se redirecciona al usuario una vez se haya passado el token

--- cashtracker/frontend/components/auth/ConfirmAccountForm.tsx
+++ cashtracker/frontend/components/auth/ConfirmAccountForm.tsx
@@ -2,18 +2,15 @@
 
 import { confirmAccount } from '@/actions/confirm-account-action'
 import { PinInput, PinInputField } from '@chakra-ui/pin-input'
-
 import { startTransition, useActionState, useEffect, useState } from 'react'
-import ErrorMessage from '../ui/ErrorMessage'
-import SuccessMessage from '../ui/SuccessMessage'
 import { toast } from 'react-toastify'
+import { useRouter } from 'next/navigation'
+
 
 export default function ConfirmAccountForm() {
 
+  const router = useRouter()
   const [isComplete, setIsComplete] = useState(false)
-
-
-
   const [token, setToken] = useState("")
 
   // .bind -> Genera una nueva funcion y en lugar de tener se pasa el confirmAccountWithToken al useActionState
@@ -47,18 +44,27 @@ export default function ConfirmAccountForm() {
   }, [isComplete])
 
   useEffect(() => {
-    if(state.errors){
+    if (state.errors) {
       state.errors.forEach(error => {
         toast.error(error)
       })
     }
-  },[state])
+
+    if (state.success) {
+      toast.success(state.success, {
+        onClose: () => {
+          router.push('/auth/login')
+        }
+      })
+    }
+  }, [state])
 
   return (
     <>
-      
-      {state.success && <SuccessMessage>{state.success}</SuccessMessage>}
-      
+      {/* 
+      {state.errors.map(error => <ErrorMessage key={error}>{error}</ErrorMessage>)}
+      {state.success && <SuccessMessage>{state.success}</SuccessMessage>} */}
+
       <div className=' flex justify-center gap-5 my-10'>
 
 
