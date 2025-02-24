ae625004e09ef502548d4ad12c9b4421c94550ee
cuauhvip07 <cuauvillalba1@gmail.com>
2025-02-23 20:38:26 -0600

    Obtener el JWT
    Se obtiene el token de las cookies

--- cashtracker/frontend/src/auth/dal.ts
+++ cashtracker/frontend/src/auth/dal.ts
@@ -1,7 +1,16 @@
 // Dal -> Data Acces Layer
 
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
 // Capa donde se ve si un usuario tiene acceso y puede acceder a los recursos 
 
 export const verifySession = async () => {
-    console.log('Desde verifySession')
+    const token = (await cookies()).get('CASHTRACKER_TOKEN')?.value
+
+    if(!token){
+        redirect('/auth/login')
+    }
+
+    console.log(token)
 }
