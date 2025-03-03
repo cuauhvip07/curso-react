3908465621e9c87ba38b953745a66be22e71d347
cuauhvip07 <cuauvillalba1@gmail.com>
2025-03-03 17:11:33 -0600

    Resetear password
    Se genera el serverAction para poder resetear el password

---
+++ cashtracker/frontend/actions/reset-password-action.ts
@@ -0,0 +1,30 @@
+"use server"
+
+import { ResetPasswordSchema } from "@/src/schemas"
+
+type ActionStateType = {
+    errors: string[],
+    success: string
+}
+
+export async function resetPassword(prevState : ActionStateType, formData: FormData) {
+    const resetPasswordInput = {
+        password: formData.get('password'),
+        password_confirmation: formData.get('password_confirmation')
+    }
+
+    const resetPassword = ResetPasswordSchema.safeParse(resetPasswordInput)
+    if(!resetPassword.success){
+        
+        return {
+            errors: resetPassword.error.errors.map(error => error.message),
+            success:''
+        }
+
+    }
+
+    return {
+        errors:[],
+        success:''
+    }
+}
--- cashtracker/frontend/src/schemas/index.ts
+++ cashtracker/frontend/src/schemas/index.ts
@@ -45,3 +45,12 @@ export const ForgotPasswordSchema = z.object({
             .min(1, {message: 'El Email es Obligatorio'})
             .email( {message: 'Email no válido'}),
 })
+
+export const ResetPasswordSchema = z.object({
+    password: z.string()
+            .min(8, {message: 'El Password debe ser de al menos 8 caracteres'}),
+    password_confirmation: z.string()
+}).refine((data) => data.password === data.password_confirmation, {
+    message: "Los Passwords no son iguales",
+    path: ["password_confirmation"]
+});
