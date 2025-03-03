17b0a631798b453e3e3a5eaa6810e8a26b84f3c9
cuauhvip07 <cuauvillalba1@gmail.com>
2025-03-03 16:33:39 -0600

    Envio de petición al back
    Se envia la petición hacia al back para la verificación del token

---
+++ cashtracker/frontend/actions/validate-token-action.ts
@@ -0,0 +1,47 @@
+"use server"
+
+import { ErrorResponseSchema, TokenSchema } from "@/src/schemas"
+
+type ActionStateType = {
+    errors: string[],
+    success: string
+}
+
+export async function validateToken(token:string,prevState:ActionStateType) {
+
+    const resetPasswordToken = TokenSchema.safeParse(token)
+
+    if(!resetPasswordToken.success){
+        return{
+            errors:resetPasswordToken.error.errors.map(error => error.message),
+            succcess:''
+        }
+    }
+
+    const url = `${process.env.API_URL}/auth/validate-token`
+
+    const req = await fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            token: resetPasswordToken.data
+        })
+    })
+    
+    const json = await req.json()
+    
+    if(!req.ok){
+        const {error} = ErrorResponseSchema.parse(json)
+        return{
+            errors:[error],
+            success:''
+        }
+    }
+
+    return{
+        errors:[],
+        success:''
+    }
+}
--- cashtracker/frontend/components/auth/ValidateTokenForm.tsx
+++ cashtracker/frontend/components/auth/ValidateTokenForm.tsx
@@ -1,18 +1,48 @@
+import { validateToken } from "@/actions/validate-token-action";
 import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import { startTransition, useActionState, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function ValidateTokenForm() {
-    const handleChange = (token: string) => {
 
+    const [token,setToken] = useState('')
+    const [isCompelte,setIsCompelte] = useState(false)
+    const validateTokenInput = validateToken.bind(null,token)
+    const [state,formAction] = useActionState(validateTokenInput,{
+        errors:[],
+        success:''
+    })
+    
+
+    const handleChange = (token: string) => {
+        setToken(token)
+        setIsCompelte(false)
     }
 
     const handleComplete = () => {
-
+        setIsCompelte(true)
     }
+
+    useEffect(() => {
+        if(isCompelte){
+            startTransition(() => {
+                formAction()
+            })
+        }
+    },[isCompelte])
+
+    useEffect(() => {
+        if(state.errors){
+            state.errors.forEach(error => {
+                toast.error(error)
+            })
+        }
+    },[state])
 
     return (
         <div className="flex justify-center gap-5 my-10">
             <PinInput
-                value={''}
+                value={token}
                 onChange={handleChange}
                 onComplete={handleComplete}
             >
