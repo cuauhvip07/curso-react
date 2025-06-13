2324ec496fc3e14269a9137830037e6c0e01bced
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-13 12:23:26 -0600

    Añadiendo logotipo al layout de auth

--- cashtracker/frontend/app/auth/layout.tsx
+++ cashtracker/frontend/app/auth/layout.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/components/iu/Logo";
 
 
 export default function AuthLayout({
@@ -9,18 +10,20 @@ export default function AuthLayout({
         <>
             <div className="lg:grid lg:grid-cols-2 lg:min-h-screen">
 
-            <div className=" bg-purple-950">
-
-            </div>
-            <div className=" p-10 lg:py-28">
-                <div className=" max-w-3xl mx-auto">
-                    {children}
+                <div className=" bg-purple-950">
+                    <div className=" w-96">
+                        <Logo />
+                    </div>
                 </div>
-            </div>
+                <div className=" p-10 lg:py-28">
+                    <div className=" max-w-3xl mx-auto">
+                        {children}
+                    </div>
+                </div>
 
             </div>
 
-            
+
         </>
 
 
