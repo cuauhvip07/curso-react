837591af1cefc95a45c1493181e37aa717e3caa9
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-13 12:45:49 -0600

    Añadiendo gráfico al layout de autenticación

--- cashtracker/frontend/app/auth/layout.tsx
+++ cashtracker/frontend/app/auth/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
         <>
             <div className="lg:grid lg:grid-cols-2 lg:min-h-screen">
 
-                <div className=" bg-purple-950 flex justify-center">
+                <div className="bg-purple-950 flex justify-center lg:bg-[url(/grafico.svg)] lg:bg-no-repeat lg:bg-[length:30rem] bg-left-bottom">
                     <div className=" w-96 py-10 lg:py-20">
                         <Logo />
                     </div>
