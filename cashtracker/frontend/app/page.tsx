2e14ed5afcdd9c92822df37433ba504bcf177bcb
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-12 21:55:58 -0600

    Eliminacion del proyecto

--- cashtracker/frontend/app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <h1 className=" text-rose-400">Hola mundo</h1>
-  );
-}
--- cashtracker/frontend/src/auth/token.ts
+++
@@ -1,5 +0,0 @@
-// import { cookies } from "next/headers";
-
-// export default async function getToken(){
-//     const token = (await cookies()).get('CASHTRACKER_TOKEN')?.value
-// }
