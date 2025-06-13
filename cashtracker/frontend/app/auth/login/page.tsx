7c66fbdd43b8dd77a4b3a6601b7eb63e4abb2d61
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-13 11:59:30 -0600

    Creación pagina login

---
+++ cashtracker/frontend/app/auth/login/page.tsx
@@ -0,0 +1,22 @@
+import LoginForm from '@/components/auth/LoginForm'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+    title: 'Cashtracker - Iniciar Sesión',
+    description: 'Cashtracker - Iniciar Sesión'
+}
+
+
+import React from 'react'
+
+export default function LoginPage() {
+    return (
+        <>
+            <h1 className="font-black text-6xl text-purple-950">Iniciar Sesión</h1>
+            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+
+            <LoginForm/>
+        </>
+    )
+}
