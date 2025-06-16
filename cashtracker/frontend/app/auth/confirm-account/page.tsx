b1e263ca66b0a5b53be78068b7002ff481622858
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-16 13:01:08 -0600

    Creación formulario para ingresar token

--- cashtracker/frontend/app/auth/confirm-account/page.tsx
+++ cashtracker/frontend/app/auth/confirm-account/page.tsx
@@ -1,11 +1,16 @@
 
 
+import ConfirmAccountForm from '@/components/auth/ConfirmAccountForm'
 import React from 'react'
 
 export default function ConfirmAccountPage() {
   return (
-    <div>
-      ConfirmAccountPage
-    </div>
+    <>
+      <h1 className="font-black text-6xl text-purple-950">Confirma tu cuenta</h1>
+      <p className="text-3xl font-bold">ingresa el código que recibiste <span className="text-amber-500">por email</span></p>
+
+      <ConfirmAccountForm/>
+
+    </>
   )
 }
