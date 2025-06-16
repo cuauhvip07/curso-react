6f0994aafa8b6a8a0dbabace68bd4a134aa71bb7
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-16 13:05:48 -0600

    Confirm Account: Guardando el token en el state

--- cashtracker/frontend/components/auth/ConfirmAccountForm.tsx
+++ cashtracker/frontend/components/auth/ConfirmAccountForm.tsx
@@ -1,11 +1,28 @@
 "use client"
 
 import {PinInput, PinInputField} from '@chakra-ui/pin-input'
+import { useActionState, useState } from 'react'
 
 export default function ConfirmAccountForm() {
+
+    const [token,setToken] = useState("")
+
+    const handleChange = (token:string) => {
+        setToken(token)
+        console.log(token)
+    }
+
+    const handleComplete = () => {
+        console.log('Llegaste al final')
+    }
+
   return (
     <div className=' flex justify-center gap-5 my-10'>
-        <PinInput>
+        <PinInput
+            value={token}
+            onChange={handleChange}
+            onComplete={handleComplete}
+        >
             <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
             <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
             <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
