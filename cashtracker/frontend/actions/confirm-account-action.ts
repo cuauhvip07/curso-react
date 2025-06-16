70041138316fa4fbc70c86849b0571b94e26bc7b
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-16 14:14:34 -0600

    ConfirmAccount: Envio de petición al back

--- cashtracker/frontend/actions/confirm-account-action.ts
+++ cashtracker/frontend/actions/confirm-account-action.ts
@@ -1,26 +1,50 @@
 "use server"
 
-import { TokenSchema } from "@/src/schemas"
+import { ErrorResponseSchema, SuccessSchema, TokenSchema } from "@/src/schemas"
+import { success } from "zod/v4"
 
 type ActionStateType = {
-    errors:string[],
-    success:string
+    errors: string[],
+    success: string
 }
 
-export async function confirmAccount(token:string,prevState:ActionStateType){
+export async function confirmAccount(token: string, prevState: ActionStateType) {
     const confirmToken = TokenSchema.safeParse(token)
 
-    if(!confirmToken.success){
-        return{
+    if (!confirmToken.success) {
+        return {
             errors: confirmToken.error.errors.map(error => error.message),
-            success:''
+            success: ''
         }
     }
 
-    console.log(confirmToken.data)
+    const url = `${process.env.API_URL}/auth/confirm-acount`
 
-    return{
-        errors:[],
-        success:''
+    const req = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            token: confirmToken.data
+        })
+    })
+
+    const json = await req.json()
+
+    if (!req.ok) {
+
+        const error = ErrorResponseSchema.parse(json)
+        return {
+            errors: [error.error],
+            success: ''
+        }
+    }
+
+    const success = SuccessSchema.parse(json)
+
+    return {
+        errors: [],
+        success
     }
 }
--- cashtracker/frontend/components/auth/ConfirmAccountForm.tsx
+++ cashtracker/frontend/components/auth/ConfirmAccountForm.tsx
@@ -1,30 +1,34 @@
 "use client"
 
 import { confirmAccount } from '@/actions/confirm-account-action'
-import {PinInput, PinInputField} from '@chakra-ui/pin-input'
+import { PinInput, PinInputField } from '@chakra-ui/pin-input'
 import { start } from 'node:repl'
 import { startTransition, useActionState, useEffect, useState } from 'react'
+import ErrorMessage from '../iu/ErrorMessage'
+import SuccessMessage from '../iu/SuccessMessage'
 
 export default function ConfirmAccountForm() {
 
-    const[isComplete,setIsComplete] = useState(false)
-    const [token,setToken] = useState("")
+    const [isComplete, setIsComplete] = useState(false)
+    const [token, setToken] = useState("")
+    const [showMessages, setShowMessages] = useState(false)
 
-    const confirmAccountWithToken = confirmAccount.bind(null,token)
-    const [state,formAction,pending] = useActionState(confirmAccountWithToken,{
-        errors:[],
-        success:''
+    const confirmAccountWithToken = confirmAccount.bind(null, token)
+    const [state, formAction, pending] = useActionState(confirmAccountWithToken, {
+        errors: [],
+        success: ''
     })
 
+
     useEffect(() => {
-        if(isComplete){
+        if (isComplete) {
             startTransition(() => {
                 formAction()
             })
         }
-    },[isComplete])
+    }, [isComplete])
 
-    const handleChange = (token:string) => {
+    const handleChange = (token: string) => {
         setToken(token)
     }
 
@@ -32,20 +36,50 @@ export default function ConfirmAccountForm() {
         setIsComplete(true)
     }
 
-  return (
-    <div className=' flex justify-center gap-5 my-10'>
-        <PinInput
-            value={token}
-            onChange={handleChange}
-            onComplete={handleComplete}
-        >
-            <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
-            <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
-            <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
-            <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
-            <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
-            <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white'/>
-        </PinInput>
-    </div>
-  )
+
+
+    useEffect(() => {
+        if (state.errors.length > 0 || state.success) {
+            setShowMessages(true)
+            const timeout = setTimeout(() => {
+                setShowMessages(false)
+            }, 5000)
+
+            return () => clearTimeout(timeout)
+        }
+    }, [state.errors, state.success])
+
+    return (
+        <>
+            {
+                showMessages && (
+                    <>
+                        {state.errors.map((error, i) => <ErrorMessage key={i}>{error}</ErrorMessage>)}
+
+                        {
+                            state.success && (
+                                <SuccessMessage>{state.success}</SuccessMessage>
+                            )
+                        }
+                    </>
+                )
+
+            }
+            <div className=' flex justify-center gap-5 my-10'>
+
+                <PinInput
+                    value={token}
+                    onChange={handleChange}
+                    onComplete={handleComplete}
+                >
+                    <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white' />
+                    <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white' />
+                    <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white' />
+                    <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white' />
+                    <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white' />
+                    <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg text-center placeholder-white' />
+                </PinInput>
+            </div>
+        </>
+    )
 }
