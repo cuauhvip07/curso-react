7fb00da26931c342d83640eab0cbb8e5af2d50bd
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-16 09:53:06 -0600

    Create account - Envio de petición al back

--- cashtracker/frontend/actions/create-account-action.ts
+++ cashtracker/frontend/actions/create-account-action.ts
@@ -2,25 +2,45 @@
 
 import { RegisterSchema } from "@/src/schemas"
 
-export async function register(formData: FormData){
-    
+export async function register(formData: FormData) {
+
     const registerData = {
-        email:formData.get('email'),
-        name:formData.get('name'),
-        password:formData.get('password'),
-        password_confirmation:formData.get('password_confirmation')
+        email: formData.get('email'),
+        name: formData.get('name'),
+        password: formData.get('password'),
+        password_confirmation: formData.get('password_confirmation')
     }
 
     // Validar
 
     const register = RegisterSchema.safeParse(registerData)
-    
+
     const errors = register.error?.errors.map(error => error.message)
+
+    if(!register.success){
+        return {}
+    }
 
     console.log(errors)
     console.log(register)
 
     // Registar 
     const url = `${process.env.API_URL}/auth/create-account`
+
+    const req = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: register.data.email,
+            name: register.data.name,
+            password: register.data.password
+        })
+    })
+
+    const json = await req.json()
+
+    console.log(json)
 
 }
