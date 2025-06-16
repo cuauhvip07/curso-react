31350dac51994be6aa260ad45f10adb80c64e708
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-16 08:53:52 -0600

    Create account - Creacion objeto para mandar al back

--- cashtracker/frontend/actions/create-account-action.ts
+++ cashtracker/frontend/actions/create-account-action.ts
@@ -1,5 +1,14 @@
 "use server"
 
-export async function register(){
-    console.log('Desde register')
+export async function register(formData: FormData){
+    
+    const registerData = {
+        email:formData.get('email'),
+        name:formData.get('name'),
+        password:formData.get('password'),
+        password_confirmation:formData.get('password_confirmation')
+    }
+
+    console.log(registerData)
+
 }
