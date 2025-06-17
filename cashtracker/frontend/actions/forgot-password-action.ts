e4c18a4b3f7cfc3713081f61be28a140e272be67
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-16 22:34:08 -0600

    Forgot Password: Envio de petición al back

--- cashtracker/frontend/actions/forgot-password-action.ts
+++ cashtracker/frontend/actions/forgot-password-action.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { ForgotPasswordSchema } from "@/src/schemas"
+import { ErrorResponseSchema, ForgotPasswordSchema, SuccessSchema } from "@/src/schemas"
 import { success } from "zod/v4"
 
 type ActionStateType = {
@@ -21,9 +21,32 @@ export async function forgotPassword(prevState:ActionStateType, formData:FormDat
         }
     }
 
+    const url = `${process.env.API_URL}/auth/forgot-password`
+
+    const req = await fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            email: forgotPassword.data.email
+        })
+    })
+
+    const json = await req.json()
+    
+    if(!req.ok){
+        const error = ErrorResponseSchema.parse(json)
+        return {
+            errors:[error.error],
+            success:''
+        }
+    }
+
+    const success = SuccessSchema.parse(json)
 
     return{
         errors:[],
-        success:''
+        success
     }
 }
