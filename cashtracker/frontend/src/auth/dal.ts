1ecb803e3ea0326cbb46f427912d1a7864ffda51
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-16 19:17:41 -0600

    Admin: Obtener el token desde las cookies

--- cashtracker/frontend/src/auth/dal.ts
+++ cashtracker/frontend/src/auth/dal.ts
@@ -1,5 +1,12 @@
 // Data Acces Layer
 
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
 export const verifySession = async () => {
-    console.log('Desde verify session')
+    const token = (await cookies()).get('CASHTRACKER_TOKEN')?.value
+
+    if(!token){
+        redirect('/auth/login')
+    }
 }
