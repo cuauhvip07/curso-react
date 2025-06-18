9c14f709c45f029c7609d762e6317caa205830c3
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-18 12:26:17 -0600

    Budgets: Listando los budgets

--- cashtracker/frontend/app/admin/page.tsx
+++ cashtracker/frontend/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import getToken from "@/src/auth/token";
 import { BudgetsAPIResponseSchema } from "@/src/schemas";
+import { formatCurrency } from "@/src/utils";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -8,13 +9,13 @@ export const metadata: Metadata = {
     description: 'Cashtracker - Panel de Administración'
 }
 
-async function getUserBudgets(){
+async function getUserBudgets() {
     const token = await getToken()
     const url = `${process.env.API_URL}/budgets`
 
-    const req = await fetch(url,{
-        headers:{
-            'Authorization':`Bearer ${token}`
+    const req = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
         },
     })
 
@@ -47,6 +48,51 @@ export default async function AdminPage() {
                     Crear Presupuesto
                 </Link>
             </div>
+
+            {budgets.length ? (
+                <>
+                    <ul role="list" className="divide-y divide-gray-300 border shadow-lg mt-10 ">
+                        {budgets.map((budget) => (
+                            <li key={budget.id} className="flex justify-between gap-x-6 p-5 ">
+                                <div className="flex min-w-0 gap-x-4">
+                                    <div className="min-w-0 flex-auto space-y-2">
+                                        <p className="text-sm font-semibold leading-6 text-gray-900">
+                                            <Link
+                                                href={`/admin/budgets/${budget.id}`}
+                                                className=" cursor-pointer hover:underline text-2xl font-bold"
+                                            >
+                                                {budget.name}
+                                            </Link>
+                                        </p>
+                                        <p className="text-xl font-bold text-amber-500">
+                                            {formatCurrency(+budget.amount)}
+                                        </p>
+                                        <p className='text-gray-500  text-sm'>
+
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="flex shrink-0 items-center gap-x-6">
+
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <>
+                    <p className="text-center py-20">
+                        No hay presupuestos aún {''}
+                        <Link
+                            href={'/admin/budgets/new'}
+                            className="text-purple-950 font-bold"
+                        >
+                            comienza creando uno
+                        </Link>
+                    </p>
+
+                </>
+            )}
         </>
     )
 }
