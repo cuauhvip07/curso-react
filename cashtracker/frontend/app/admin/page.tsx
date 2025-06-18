d166d214a729a0864e50985db7898262ef5dded9
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-18 17:18:59 -0600

    Admin: Mostrando fecha del presupuesto

--- cashtracker/frontend/app/admin/page.tsx
+++ cashtracker/frontend/app/admin/page.tsx
@@ -1,6 +1,6 @@
 import getToken from "@/src/auth/token";
 import { BudgetsAPIResponseSchema } from "@/src/schemas";
-import { formatCurrency } from "@/src/utils";
+import { formatCurrency, formatDate } from "@/src/utils";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -68,7 +68,8 @@ export default async function AdminPage() {
                                             {formatCurrency(+budget.amount)}
                                         </p>
                                         <p className='text-gray-500  text-sm'>
-
+                                            Última actualización: {''}
+                                            <span className=" font-bold">{formatDate( budget.updatedAt)}</span>
                                         </p>
                                     </div>
                                 </div>
