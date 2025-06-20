a8f9904d97783567b7ca6587dd0a16bf2b0667ec
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-19 21:40:24 -0600

    Edit Budget: Re-utilizando el formulario de create budget

--- cashtracker/frontend/components/budgets/CreateBudgetForm.tsx
+++ cashtracker/frontend/components/budgets/CreateBudgetForm.tsx
@@ -6,6 +6,7 @@ import ErrorMessage from "../iu/ErrorMessage"
 import SuccessMessage from "../iu/SuccessMessage"
 import { toast } from "react-toastify"
 import { useRouter } from "next/navigation"
+import BudgetForm from "./BudgetForm"
 
 export default function CreateBudgetForm() {
 
@@ -48,35 +49,10 @@ export default function CreateBudgetForm() {
                         {state.errors.map((error, i) => <ErrorMessage key={i}>{error}</ErrorMessage>)}
                     </>
                 )
-
-
-
             }
 
-            <div className="space-y-3">
-                <label htmlFor="name" className="text-sm uppercase font-bold">
-                    Nombre Presupuesto
-                </label>
-                <input
-                    id="name"
-                    className="w-full p-3  border border-gray-100 bg-slate-100"
-                    type="text"
-                    placeholder="Nombre del Presupuesto"
-                    name="name"
-                />
-            </div>
-            <div className="space-y-3">
-                <label htmlFor="amount" className="text-sm uppercase font-bold">
-                    Cantidad Presupuesto
-                </label>
-                <input
-                    type="number"
-                    id="amount"
-                    className="w-full p-3  border border-gray-100 bg-slate-100"
-                    placeholder="Cantidad Presupuesto"
-                    name="amount"
-                />
-            </div>
+            <BudgetForm/>
+
             <button
                 type="submit"
                 className="bg-amber-500 w-full p-3 text-white uppercase font-bold hover:bg-amber-600 cursor-pointer transition-colors"
---
+++ cashtracker/frontend/components/budgets/EditBudgetForm.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import BudgetForm from "./BudgetForm";
+
+export default function EditBudgetForm() {
+    return (
+        <form
+            className="mt-10 space-y-3"
+            noValidate
+            // action={formAction}
+        >
+
+            {/* {
+                showMessages && (
+                    <>
+                        {state.errors.map((error, i) => <ErrorMessage key={i}>{error}</ErrorMessage>)}
+                    </>
+                )
+            } */}
+
+            <BudgetForm />
+
+            {/* <button
+                type="submit"
+                className="bg-amber-500 w-full p-3 text-white uppercase font-bold hover:bg-amber-600 cursor-pointer transition-colors"
+                // disabled={pending}
+            >
+                {pending ? (
+                    <div className="flex items-center justify-center gap-2">
+                        <span className="h-5 w-5 animate-spin rounded-full border-2 border-white border-t-transparent" />
+                        Cargando...
+                    </div>
+                ) : (
+                    "Crear"
+                )}
+            </button> */}
+
+            <button type="submit">hola</button>
+        </form>
+    )
+}
