fae09a9dd3b047e84bebcc77c27898212c24963b
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-24 18:42:21 -0600

    Budget: Obteniendo los budgets

---
+++ cashtracker/frontend/app/admin/budgets/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { getBudgetById } from "@/src/services/budgets"
+import { Metadata } from "next"
+
+export async function generateMetadata({params} : {params:{id:string}}): Promise<Metadata> {
+
+  const {id} = await params
+
+  const budget = await getBudgetById(id)
+
+  return {
+    title:`Cashtracker - ${budget.name}`,
+    description:`Cashtracker - ${budget.name}`
+  }
+
+}
+
+
+export default async function BudgetDetailsPage({ params }: { params: { id: string } }) {
+
+    const { id } = await params
+
+    const budget = await getBudgetById(id)
+    console.log(budget)
+
+    return (
+        <>
+            <div className='flex justify-between items-center'>
+                <div>
+                    <h1 className="font-black text-4xl text-purple-950">{budget.name}</h1>
+                    <p className="text-xl font-bold">Administra tus {''} <span className="text-amber-500">gastos</span></p>
+                </div>
+
+            </div>
+        </>
+    )
+}
