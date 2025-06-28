fc605b2a6691e4f922714976289be07fca390a06
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-27 23:13:12 -0600

    Expense: Mostrando los expenses

--- cashtracker/frontend/app/admin/budgets/[id]/page.tsx
+++ cashtracker/frontend/app/admin/budgets/[id]/page.tsx
@@ -1,18 +1,19 @@
 import AddExpenseButton from "@/components/expenses/AddExpenseButton"
 import ModalContainer from "@/components/iu/ModalContainer"
 import { getBudgetById } from "@/src/services/budgets"
+import { formatCurrency, formatDate } from "@/src/utils"
 import { Metadata } from "next"
 
-export async function generateMetadata({params} : {params:{id:string}}): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
 
-  const {id} = await params
+    const { id } = await params
 
-  const budget = await getBudgetById(id)
+    const budget = await getBudgetById(id)
 
-  return {
-    title:`Cashtracker - ${budget.name}`,
-    description:`Cashtracker - ${budget.name}`
-  }
+    return {
+        title: `Cashtracker - ${budget.name}`,
+        description: `Cashtracker - ${budget.name}`
+    }
 
 }
 
@@ -32,10 +33,44 @@ export default async function BudgetDetailsPage({ params }: { params: { id: stri
                     <h1 className="font-black text-4xl text-purple-950">{budget.name}</h1>
                     <p className="text-xl font-bold">Administra tus {''} <span className="text-amber-500">gastos</span></p>
                 </div>
-                <AddExpenseButton/>
+                <AddExpenseButton />
             </div>
 
-            <ModalContainer/>
+            {budget.expenses.length ? (
+                <>
+                    <h1 className=" font-black text-4xl text-purple-950 mt-10">
+                        Gastos en este presupuesto
+                    </h1>
+
+                    <ul role="list" className="divide-y divide-gray-300 border shadow-lg mt-10 ">
+                        {budget.expenses.map((expense) => (
+                            <li key={expense.id} className="flex justify-between gap-x-6 p-5">
+                                <div className="flex min-w-0 gap-x-4">
+                                    <div className="min-w-0 flex-auto space-y-2">
+                                        <p className="text-2xl font-semibold text-gray-900">
+                                            {expense.name}
+                                        </p>
+                                        <p className="text-xl font-bold text-amber-500">
+                                            {formatCurrency(+expense.amount)}
+                                        </p>
+                                        <p className='text-gray-500  text-sm'>
+                                            Agregado: {''}
+                                            <span className=" font-bold">{formatDate(expense.updatedAt)}</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+
+                </>
+            ) : (
+                <>
+                    <p className=" text-center py-20">No hay gastos aún</p>
+                </>
+            )}
+
+            <ModalContainer />
 
         </>
     )
