b832bb46df707287298f4ecd0fb7f171ceb5a5cb
Cuauhtemoc Villalba <cuauvillalba1@gmail.com>
2025-06-28 20:20:47 -0600

    Consultando la API para obtener presupuesto

--- cashtracker/frontend/app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
+++ cashtracker/frontend/app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,9 +1,27 @@
 import { verifySession } from "@/src/auth/dal"
+import getToken from "@/src/auth/token";
 
 
-export async function GET(){
+export async function GET(request: Request,{ params }: { params: { budgetId: string; expenseId: string } }){
 
     await verifySession()
+    
+    const paramsAwait = await params
+    const {budgetId,expenseId} = paramsAwait
+    
+    const token = await getToken()
+    const url = `${process.env.API_URL}/budgets/${budgetId}/expenses/${expenseId}`
+    const req = await fetch(url,{
+        headers:{
+            'Authorization':`Bearer ${token}`
+        }
+    })
 
-    return Response.json('Hola mundo')
+    const json = await req.json()
+
+    if(!req.ok){
+        return Response.json(json.error, {status:403})
+    }
+
+    return Response.json(json)
 }
